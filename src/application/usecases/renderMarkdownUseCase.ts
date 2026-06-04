f26b5b3834bab0900bab9fdadccf0b858773e2af
yanagikad <thekykun+git@gmail.com>
2026-06-04 14:04:45 +0900

    clean

--- src/application/usecases/renderMarkdownUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { MarkdownRenderer } from "../../domain/ports/markdownRenderer";
-
-export class RenderMarkdownUseCase {
-  constructor(private readonly renderer: MarkdownRenderer) {}
-
-  execute(markdown: string): string {
-    return this.renderer.render(markdown);
-  }
-}
--- src/domain/ports/markdownRenderer.ts
+++
@@ -1,3 +0,0 @@
-export interface MarkdownRenderer {
-  render(markdown: string): string;
-}
